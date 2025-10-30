9575b51f51e5d3eb0af31e498e91b79f569558ae
Rodolfo Díaz <rodolfo.diaz.rovira@gmail.com>
2025-10-29 21:08:18 -0500

    Add GoDoc comment for Product struct.

--- models/product.go
+++ models/product.go
@@ -1,5 +1,7 @@
 package models
 
+// Product represents an item available for purchase.
+// It includes a unique identifier, a name, and a price.
 type Product struct {
 	ID    string
 	Name  string
